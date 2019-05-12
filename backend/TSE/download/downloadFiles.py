ad5ec28b2b7eee406e2a0f0f9082bbad05f5ff81
Jean Schuchardt <jeanschuchardt@hotmail.com>
2019-05-12 16:55:46 -0300

    donwload dados TSE

---
+++ backend/TSE/download/downloadFiles.py
@@ -0,0 +1,28 @@
+import urllib3
+import os
+import shutil
+
+def downloader(url,estadosL,partidosL):
+    for partido in partidosL:
+        for estado in estadosL:
+            urlFinal = url+partido+'_'+estado+'.zip'
+            print(urlFinal)
+            download(urlFinal,partido,estado)
+
+            # remover para somente usar isso para fazer o download #
+            # manipula.unziper()
+            # manipula.organizaF()
+
+def download(urlFinal,p,e):
+    c = urllib3.PoolManager()
+    path = creatFolders('downloads')
+    filename = path+'/' +p+'_'+e+'.zip'
+    with c.request('GET',urlFinal, preload_content=False) as resp, open(filename, 'wb') as out_file:
+        shutil.copyfileobj(resp, out_file)
+    resp.release_conn() 
+
+def creatFolders(folderName):
+    path = os.getcwd() + '/' + folderName
+    if not os.path.exists(path):
+         os.mkdir(path)
+    return path
