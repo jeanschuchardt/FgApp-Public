bcf3a5e3db67a0f33a421e999f912c9fdccab4fa
Jean_Burda <jean_burda@dell.com>
2019-08-27 21:48:52 -0300

    quebra de metodos

--- backend/Servidores/inserter/insert.py
+++ backend/Servidores/inserter/insert.py
@@ -5,66 +5,77 @@ from datetime import datetime
 
 
 def insert_cadastro(cadastro,remuneracao):
-
-
-    c = 10000
-
-    y = read_csv(remuneracao)
-    y.columns = remove_pt_caracteres(y)
-    y = remove_coll_dolar(y)
-
-
-
-    remove_spaces(y)
-
-
-    y = keep_columns(y)
-
-    print ()
-
-
-
+    chunk = 10000
+    #le o arquivo csv remuneração
+    remuneracao_result_set = read_csv(remuneracao)
     
+    #remove caracteres especiais da lingua portuguesa 
+    remuneracao_result_set.columns = remove_pt_caracteres(remuneracao_result_set)
+    
+    #remove dolar 
+    remuneracao_result_set = remove_coll_dolar(remuneracao_result_set)
+    
+    #remove espaços em branco
+    remove_spaces(remuneracao_result_set)
+
+    #remove colunas não necessarias 
+    remuneracao_result_set = remove_columns_remuneracao(remuneracao_result_set)
+    
+    #cria nova coluna para armazenar o gato total do servidor
+    create_total_column(remuneracao_result_set)
+
+    #keep_col = ['ANO','MES','Id_SERVIDOR_PORTAL','CPF','NOME','total_remuneracao'] #remover valores intermediarios a soma 
+    #y = y[keep_col]
+    ###
+
+    for i, result_set in enumerate((pd.read_csv(cadastro, encoding ="ISO-8859-1", delimiter=';', skipinitialspace=True,error_bad_lines = False, engine='c',chunksize=chunk))):
+        
+        result_set = remove_columns_cadastro(result_set)
+
+        
+        result_set = remove_last_line(result_set)
+
+        result_set = define_columns_types(result_set)
+        
+        merge= pd.merge(result_set,remuneracao_result_set,how='inner',on=['Id_SERVIDOR_PORTAL','NOME','CPF'])
+
+        
+        merge.to_csv("D:\\Github\\FGApp\\backend\\Servidores\\test\\"+  str(i) + datetime.now().strftime("%d-%m-%Y-%H-%M-%S") +'.csv', index=False)
+##
+# rdefine tipos de dados e remove espaços em branco
+# #
+def define_columns_types(resul_set):
+    resul_set.CPF = resul_set.CPF.str.replace('\D','')
+    resul_set.MATRICULA = resul_set.MATRICULA.str.replace('\D','')
+    resul_set["Id_SERVIDOR_PORTAL"] = resul_set['Id_SERVIDOR_PORTAL'].astype('int')
+    resul_set["CPF"] = resul_set['CPF'].astype('int')
+    resul_set["MATRICULA"] = resul_set['MATRICULA'].astype('int')
+    return resul_set
+
+def remove_last_line(x):
+    x = x[x.SIGLA_FUNCAO != '-1']
+    return x
+
+def remove_columns_cadastro(x):
+    keep_col = ['Id_SERVIDOR_PORTAL','NOME','CPF','MATRICULA','SIGLA_FUNCAO',
+                'NIVEL_FUNCAO','FUNCAO','UORG_EXERCICIO','DATA_INICIO_AFASTAMENTO',
+                'DATA_TERMINO_AFASTAMENTO','DATA_INGRESSO_CARGOFUNCAO','UF_EXERCICIO']
 
 
-    col_list = list(y)
+    x = x[keep_col]
+    return x
+
+def create_total_column(remuneracao_result_set):
+    col_list = list(remuneracao_result_set)
     col_list.remove('ANO')
     col_list.remove('MES')
     col_list.remove('Id_SERVIDOR_PORTAL')
     col_list.remove('CPF')
     col_list.remove('NOME')
 
-    define_types(y)
+    define_types(remuneracao_result_set)
 
-    y['total_remuneracao'] = y[col_list].sum(axis=1)
-
-
-    #y.to_csv("D:\\Github\\FGApp\\backend\\Servidores\\test\\"+  'test_soma' +'.csv', index=False)
-
-    #keep_col = ['ANO','MES','Id_SERVIDOR_PORTAL','CPF','NOME','total_remuneracao'] #remover valores intermediarios a soma 
-    #y = y[keep_col]
-    ###
-
-
-    for i, x in enumerate((pd.read_csv(cadastro, encoding ="ISO-8859-1", delimiter=';', skipinitialspace=True,error_bad_lines = False, engine='c',chunksize=c))):
-        keep_col = ['Id_SERVIDOR_PORTAL','NOME','CPF','MATRICULA','SIGLA_FUNCAO',
-                    'NIVEL_FUNCAO','FUNCAO','UORG_EXERCICIO','DATA_INICIO_AFASTAMENTO',
-                    'DATA_TERMINO_AFASTAMENTO','DATA_INGRESSO_CARGOFUNCAO','UF_EXERCICIO']
-
-
-        x = x[keep_col]
-        x = x[x.SIGLA_FUNCAO != '-1']
-
-        x.CPF = x.CPF.str.replace('\D','')
-        x.MATRICULA = x.MATRICULA.str.replace('\D','')
-        x["Id_SERVIDOR_PORTAL"] = x['Id_SERVIDOR_PORTAL'].astype('int')
-        x["CPF"] = x['CPF'].astype('int')
-        x["MATRICULA"] = x['MATRICULA'].astype('int')
-        
-        merge= pd.merge(x,y,how='inner',on=['Id_SERVIDOR_PORTAL','NOME','CPF'])
-
-        
-        merge.to_csv("D:\\Github\\FGApp\\backend\\Servidores\\test\\"+  str(i) + datetime.now().strftime("%d-%m-%Y-%H-%M-%S") +'.csv', index=False)
+    remuneracao_result_set['total_remuneracao'] = remuneracao_result_set[col_list].sum(axis=1)
 
 def define_types(y):
     y["ANO"] = y['ANO'].astype('int')
@@ -78,7 +89,7 @@ def define_types(y):
     y["OUTRAS_REMUNERACOES_EVENTUAIS"] = y['OUTRAS_REMUNERACOES_EVENTUAIS'].astype('float')
     y["TOTAL_DE_VERBAS_INDENIZATORIAS"] = y['TOTAL_DE_VERBAS_INDENIZATORIAS'].astype('float')
 
-def keep_columns(y):
+def remove_columns_remuneracao(y):
     keep_col =      ['ANO', 'MES', 'Id_SERVIDOR_PORTAL', 'CPF', 'NOME',
                         'REMUNERACAO_BASICA_BRUTA', 'GRATIFICACAO_NATALINA',
                         'FERIAS',
