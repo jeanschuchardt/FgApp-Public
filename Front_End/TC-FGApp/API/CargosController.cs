ca8a6cbed0e3f40c029f7025405ea773e31f385a
joaoberte <joaoberte2006@yahoo.com.br>
2019-10-20 23:22:37 -0300

    API de Cargos

--- Front_End/TC-FGApp/API/CargosController.cs
+++ Front_End/TC-FGApp/API/CargosController.cs
@@ -3,14 +3,13 @@ using DataBaseFramework.Context;
 using DataBaseFramework.DataModel;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
-using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace FGApp.API
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     public class CargosController : Controller
     {
         private ConnectionStringsConfig _connectionStrings;
@@ -27,33 +26,65 @@ namespace FGApp.API
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/<controller>/5
         [HttpGet("{ano}")]
-        public JsonResult Get(int ano)
+        public JsonResult GetServidoresPartidos(int ano)
         {
-            List<int> listaAnos = new RegioesCargosBO(_connectionStrings.DefaultConnection).GetAllDataCargosDisponiveis();
-
             List<RegioesCargos> listaPartidoServidores = new RegioesCargosBO(_connectionStrings.DefaultConnection).GetServidoresPorPartido(new RegioesCargos() { DataCargos = new DateTime(ano, 1, 1) });
 
-            return Json(listaPartidoServidores.Select(x => new { x.Partido, x.TotalCargos }).ToArray());
+            if (listaPartidoServidores.Count > 0)
+            {
+                return Json(listaPartidoServidores.Select(x => new { x.Partido, x.TotalCargos }).ToArray());
+            }
+            else
+            {
+                return Json(null);
+            }
         }
 
-        // POST api/<controller>
-        [HttpPost]
-        public void Post([FromBody]string value)
+        [HttpGet("{ano}")]
+        public JsonResult GetDistribuicaoFuncoes(int ano)
         {
+            List<RegioesCargos> listaPartidoServidores = new RegioesCargosBO(_connectionStrings.DefaultConnection).GetDistribuicaoFuncoes(new RegioesCargos() { DataCargos = new DateTime(ano, 1, 1) });
+
+            if (listaPartidoServidores.Count > 0)
+            {
+                return Json(listaPartidoServidores.Select(x => new { x.Estado, x.TotalCargos }).ToArray());
+            }
+            else
+            {
+                return Json(null);
+            }
         }
 
-        // PUT api/<controller>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        
+        [HttpGet("{estado}/{tipoCargo}")]
+        public JsonResult GetServidoresPorAno(string estado, string tipoCargo)
         {
+            List<RegioesCargos> listaServidoresPorAno = new RegioesCargosBO(_connectionStrings.DefaultConnection).GetServidoresPorAno(new RegioesCargos() { Estado = estado, TipoCargos = tipoCargo});
+
+            if (listaServidoresPorAno.Count > 0)
+            {
+                return Json(listaServidoresPorAno.Select(x => new { x.DataCargos.Year, x.TipoCargos, x.TotalCargos}).ToArray());
+            }
+            else
+            {
+                return Json(null);
+            }
         }
 
-        // DELETE api/<controller>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        [HttpGet]
+        public JsonResult GetAllRegioesCargos()
         {
+            List<RegioesCargos> listaRegioesCargos = new RegioesCargosBO(_connectionStrings.DefaultConnection).GetAllRegioesCargos();
+
+            if (listaRegioesCargos.Count > 0)
+            {
+                return Json(listaRegioesCargos.Select(x => new { x.Estado, x.Cidade, x.DataCargos.Date, x.Partido, x.TipoCargos, x.TotalCargos }).ToArray());
+            }
+            else
+            {
+                return Json(null);
+            }
         }
     }
 }
