1f5e9d87dcfcf1edd0d268d8a16dd6c7f3684f35
joaoberte <joaoberte2006@yahoo.com.br>
2019-11-09 19:10:56 -0300

    media e relação ano transposto

--- Front_End/TC-FGApp/wwwroot/js/chart.js
+++ Front_End/TC-FGApp/wwwroot/js/chart.js
@@ -103,7 +103,7 @@ $(function () {
         maintainAspectRatio: false
     };
 
-    var doughnutPieData = {
+    var PieData = {
         datasets: [{
             data: [3224, 2811, 2055, 1678, 1408],
             backgroundColor: [
@@ -122,6 +122,32 @@ $(function () {
             'PP'
         ]
     };
+    var PieOptions = {
+        responsive: true,
+        animation: {
+            animateScale: true,
+            animateRotate: true
+        },
+        legend: {
+            display: true,
+            position: 'right'
+        },
+        maintainAspectRatio: false
+    };
+
+    var doughnutPieData = {
+        datasets: [{
+            data: [98057, 22492],
+            backgroundColor: [
+                '#22FF2277',
+                '#2222FF77'
+            ],
+        }],
+        labels: [
+            'Servidores',
+            'Relação'
+        ]
+    };
     var doughnutPieOptions = {
         responsive: true,
         animation: {
@@ -134,6 +160,7 @@ $(function () {
         },
         maintainAspectRatio: false
     };
+
     var areaData = {
         labels: ["Jan/15", "Fev/15", "Mar/15", "Abr/15", "Mai/15", "Jun/15", "Jul/15", "Ago/15", "Set/15", "Out/15", "Nov/15", "Dez/15",
             "Jan/16", "Fev/16", "Mar/16", "Abr/16", "Mai/16", "Jun/16", "Jul/16", "Ago/16", "Set/16", "Out/16", "Nov/16", "Dez/16",
@@ -182,6 +209,72 @@ $(function () {
         maintainAspectRatio: false
     }
 
+    var transpostoData = {
+        labels: ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"],
+        datasets: [
+            {
+                label: "2018",
+                data: [22676, 22610, 22569, 22549, 22598, 22339, 22547, 22583, 22426, 22158, 22177, 22272],
+                backgroundColor: '#9a55ff55',
+                borderColor: '#9a55ff',
+                borderWidth: 2,
+                fill: false,
+                pointRadius: 5,
+                pointHoverRadius: 7,
+                pointHitRadius: 25,
+                pointBackgroundColor: '#9a55ffCC',
+            },
+            {
+                label: "2017",
+                data: [22909, 23566, 23430, 22948, 23772, 23436, 23479, 23511, 23430, 23439, 23371, 23491],
+                backgroundColor: '#07cdae55',
+                borderColor: '#07cdae',
+                borderWidth: 2,
+                fill: false,
+                pointRadius: 5,
+                pointHoverRadius: 7,
+                pointHitRadius: 25,
+                pointBackgroundColor: '#07cdaeCC',
+            },
+            {
+                label: "2016",
+                data: [22898, 22812, 22897, 22658, 22560, 22534, 22548, 22502, 22477, 22422, 22367, 22422,],
+                backgroundColor: '#047edf55',
+                borderColor: '#047edf',
+                borderWidth: 2,
+                fill: false,
+                pointRadius: 5,
+                pointHoverRadius: 7,
+                pointHitRadius: 25,
+                pointBackgroundColor: '#047edfCC',
+            },
+            {
+                label: "2015",
+                data: [23823, 23745, 23814, 23810, 23699, 23495, 23348, 23168, 23144, 23024, 22930, 22874],
+                backgroundColor: '#fe709655',
+                borderColor: '#fe7096',
+                borderWidth: 2,
+                fill: false,
+                pointRadius: 5,
+                pointHoverRadius: 7,
+                pointHitRadius: 25,
+                pointBackgroundColor: '#fe7096CC',
+            }
+        ]
+    };
+
+    var transpostoOptions = {
+        plugins: {
+            filler: {
+                propagate: true
+            }
+        },
+        legend: {
+            display: false
+        },
+        maintainAspectRatio: false
+    }
+
     // Get context with jQuery - using jQuery's .get() method.
     if ($("#barChart").length) {
         var barChartCanvas = $("#barChart").get(0).getContext("2d");
@@ -215,8 +308,8 @@ $(function () {
         var pieChartCanvas = $("#pieChart").get(0).getContext("2d");
         var pieChart = new Chart(pieChartCanvas, {
             type: 'pie',
-            data: doughnutPieData,
-            options: doughnutPieOptions
+            data: PieData,
+            options: PieOptions
         });
     }
 
@@ -226,6 +319,15 @@ $(function () {
             type: 'line',
             data: areaData,
             options: areaOptions
+        });
+    }
+
+    if ($("#transpostoChart").length) {
+        var areaChartCanvas = $("#transpostoChart").get(0).getContext("2d");
+        var areaChart = new Chart(areaChartCanvas, {
+            type: 'line',
+            data: transpostoData,
+            options: transpostoOptions
         });
     }
 });
