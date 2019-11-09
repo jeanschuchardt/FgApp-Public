0beada6efb08362da1217fe50245bf7974091d44
joaoberte <joaoberte2006@yahoo.com.br>
2019-11-09 18:49:31 -0300

    grafico de gastos

--- Front_End/TC-FGApp/wwwroot/js/chart.js
+++ Front_End/TC-FGApp/wwwroot/js/chart.js
@@ -47,37 +47,6 @@ $(function () {
             fill: false
         }]
     };
-    var multiLineData = {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [{
-            label: 'Dataset 1',
-            data: [12, 19, 3, 5, 2, 3],
-            borderColor: [
-                '#587ce4'
-            ],
-            borderWidth: 2,
-            fill: false
-        },
-        {
-            label: 'Dataset 2',
-            data: [5, 23, 7, 12, 42, 23],
-            borderColor: [
-                '#ede190'
-            ],
-            borderWidth: 2,
-            fill: false
-        },
-        {
-            label: 'Dataset 3',
-            data: [15, 10, 21, 32, 12, 33],
-            borderColor: [
-                '#f44252'
-            ],
-            borderWidth: 2,
-            fill: false
-        }
-        ]
-    };
     var options = {
         scales: {
             yAxes: [{
@@ -97,6 +66,43 @@ $(function () {
         },
         maintainAspectRatio: false
     };
+
+    var gastoData = {
+        labels: ["2013", "2014", "2015", "2016", "2017", "2018", "2019"],
+        datasets: [
+            {
+                label: 'Total em R$',
+                data: [12198301475, 13826453355, 15132703247, 15945470761, 17486948219, 16621539654, 4188528584],
+                backgroundColor: '#ffd50088',
+                borderColor: '#ffd500',
+                borderWidth: 1,
+                fill: true,
+                pointRadius: 5,
+                pointHoverRadius: 7,
+                pointHitRadius: 25,
+                pointBackgroundColor: '#94c2ffCC',
+            },
+        ]
+    };
+    var gastoOptions = {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        },
+        legend: {
+            display: false
+        },
+        elements: {
+            point: {
+                radius: 0
+            }
+        },
+        maintainAspectRatio: false
+    };
+
     var doughnutPieData = {
         datasets: [{
             data: [3224, 2811, 2055, 1678, 1408],
@@ -176,129 +182,6 @@ $(function () {
         maintainAspectRatio: false
     }
 
-    var multiAreaData = {
-        labels: ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"],
-        datasets: [{
-            label: 'Facebook',
-            data: [8, 11, 13, 15, 12, 13, 16, 15, 13, 19, 11, 14],
-            borderColor: ['rgba(255, 99, 132, 0.5)'],
-            backgroundColor: ['rgba(255, 99, 132, 0.5)'],
-            borderWidth: 1,
-            fill: true
-        },
-        {
-            label: 'Twitter',
-            data: [7, 17, 12, 16, 14, 18, 16, 12, 15, 11, 13, 9],
-            borderColor: ['rgba(54, 162, 235, 0.5)'],
-            backgroundColor: ['rgba(54, 162, 235, 0.5)'],
-            borderWidth: 1,
-            fill: true
-        },
-        {
-            label: 'Linkedin',
-            data: [6, 14, 16, 20, 12, 18, 15, 12, 17, 19, 15, 11],
-            borderColor: ['rgba(255, 206, 86, 0.5)'],
-            backgroundColor: ['rgba(255, 206, 86, 0.5)'],
-            borderWidth: 1,
-            fill: true
-        }
-        ]
-    };
-
-    var multiAreaOptions = {
-        plugins: {
-            filler: {
-                propagate: true
-            }
-        },
-        elements: {
-            point: {
-                radius: 0
-            }
-        },
-        scales: {
-            xAxes: [{
-                gridLines: {
-                    display: false
-                }
-            }],
-            yAxes: [{
-                gridLines: {
-                    display: false
-                }
-            }]
-        },
-        maintainAspectRatio: false
-    }
-
-    var scatterChartData = {
-        datasets: [{
-            label: 'First Dataset',
-            data: [{
-                x: -10,
-                y: 0
-            },
-            {
-                x: 0,
-                y: 3
-            },
-            {
-                x: -25,
-                y: 5
-            },
-            {
-                x: 40,
-                y: 5
-            }
-            ],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)'
-            ],
-            borderWidth: 1
-        },
-        {
-            label: 'Second Dataset',
-            data: [{
-                x: 10,
-                y: 5
-            },
-            {
-                x: 20,
-                y: -30
-            },
-            {
-                x: -25,
-                y: 15
-            },
-            {
-                x: -10,
-                y: 5
-            }
-            ],
-            backgroundColor: [
-                'rgba(54, 162, 235, 0.2)',
-            ],
-            borderColor: [
-                'rgba(54, 162, 235, 1)',
-            ],
-            borderWidth: 1
-        }
-        ]
-    }
-
-    var scatterChartOptions = {
-        scales: {
-            xAxes: [{
-                type: 'linear',
-                position: 'bottom'
-            }]
-        },
-        maintainAspectRatio: false
-    }
-
     // Get context with jQuery - using jQuery's .get() method.
     if ($("#barChart").length) {
         var barChartCanvas = $("#barChart").get(0).getContext("2d");
@@ -310,30 +193,12 @@ $(function () {
         });
     }
 
-    if ($("#lineChart").length) {
-        var lineChartCanvas = $("#lineChart").get(0).getContext("2d");
+    if ($("#gastoChart").length) {
+        var lineChartCanvas = $("#gastoChart").get(0).getContext("2d");
         var lineChart = new Chart(lineChartCanvas, {
-            type: 'line',
-            data: data,
-            options: options
-        });
-    }
-
-    if ($("#linechart-multi").length) {
-        var multiLineCanvas = $("#linechart-multi").get(0).getContext("2d");
-        var lineChart = new Chart(multiLineCanvas, {
-            type: 'line',
-            data: multiLineData,
-            options: options
-        });
-    }
-
-    if ($("#areachart-multi").length) {
-        var multiAreaCanvas = $("#areachart-multi").get(0).getContext("2d");
-        var multiAreaChart = new Chart(multiAreaCanvas, {
-            type: 'line',
-            data: multiAreaData,
-            options: multiAreaOptions
+            type: 'bar',
+            data: gastoData,
+            options: gastoOptions
         });
     }
 
@@ -361,24 +226,6 @@ $(function () {
             type: 'line',
             data: areaData,
             options: areaOptions
-        });
-    }
-
-    if ($("#scatterChart").length) {
-        var scatterChartCanvas = $("#scatterChart").get(0).getContext("2d");
-        var scatterChart = new Chart(scatterChartCanvas, {
-            type: 'scatter',
-            data: scatterChartData,
-            options: scatterChartOptions
-        });
-    }
-
-    if ($("#browserTrafficChart").length) {
-        var doughnutChartCanvas = $("#browserTrafficChart").get(0).getContext("2d");
-        var doughnutChart = new Chart(doughnutChartCanvas, {
-            type: 'doughnut',
-            data: browserTrafficData,
-            options: doughnutPieOptions
         });
     }
 });
