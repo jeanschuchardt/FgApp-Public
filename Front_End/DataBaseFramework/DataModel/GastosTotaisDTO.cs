86065fe648600ee5ab0f7fbc71a18254205eeb6f
joaoberte <joaoberte2006@yahoo.com.br>
2019-11-10 14:29:54 -0300

    dto

---
+++ Front_End/DataBaseFramework/DataModel/GastosTotaisDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataBaseFramework.DataModel
+{
+    public class GastosTotaisDTO
+    {
+        #region colunasTabela
+
+        public int Mes { get; set; }
+
+        public int Ano { get; set; }
+
+        public string SiglaFuncao { get; set; }
+
+        public double TotalRemuneracao { get; set; }
+
+        #endregion
+    }
+}
